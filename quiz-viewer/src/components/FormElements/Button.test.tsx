5eff8a05760b4f0c8dfbfee3f7281d487ae9444d
Shawn Mealey <shawn@foratravel.com>
2025-09-11 08:50:36 -0400

    Trying to fix type issues

--- quiz-viewer/src/components/FormElements/Button.test.tsx
+++ quiz-viewer/src/components/FormElements/Button.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { Button, variantClasses } from "./Button";
 
 describe("Button Component", () => {
